382b2095a2992952f1d7828b0808fad70b50a762
mygitprojectss <anushiyamurugesan97@gmail.com>
2026-03-11 16:13:50 +0530

    add trained models

--- src/prediction/predict_ml.py
+++ src/prediction/predict_ml.py
@@ -1,30 +1,15 @@
-import os
 import pickle
 
-from src.preprocessing.clean_text import clean_text
-
-MODEL_PATH = "models/ml_model.pkl"
-VEC_PATH = "models/vectorizer.pkl"
-
-# If model not exist -> train automatically
-if not os.path.exists(MODEL_PATH):
-
-    from src.training.train_ml import train_model
-    train_model()
-
-with open(MODEL_PATH, "rb") as f:
+with open("models/ml_model.pkl","rb") as f:
     model = pickle.load(f)
 
-with open(VEC_PATH, "rb") as f:
+with open("models/vectorizer.pkl","rb") as f:
     vectorizer = pickle.load(f)
-
 
 def predict(text):
 
-    text = clean_text(text)
+    text_vec = vectorizer.transform([text])
 
-    vec = vectorizer.transform([text])
-
-    prediction = model.predict(vec)[0]
+    prediction = model.predict(text_vec)[0]
 
     return prediction
