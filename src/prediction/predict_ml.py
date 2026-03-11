e10d11d5db2ad3429501c1151242a8e27471e3b1
mygitprojectss <anushiyamurugesan97@gmail.com>
2026-03-11 16:04:46 +0530

    auto training fix

--- src/prediction/predict_ml.py
+++ src/prediction/predict_ml.py
@@ -3,18 +3,19 @@ import pickle
 
 from src.preprocessing.clean_text import clean_text
 
+MODEL_PATH = "models/ml_model.pkl"
+VEC_PATH = "models/vectorizer.pkl"
 
-# auto train if model not present
-if not os.path.exists("models/ml_model.pkl"):
+# If model not exist -> train automatically
+if not os.path.exists(MODEL_PATH):
 
     from src.training.train_ml import train_model
     train_model()
 
-
-with open("models/ml_model.pkl","rb") as f:
+with open(MODEL_PATH, "rb") as f:
     model = pickle.load(f)
 
-with open("models/vectorizer.pkl","rb") as f:
+with open(VEC_PATH, "rb") as f:
     vectorizer = pickle.load(f)
 
 
